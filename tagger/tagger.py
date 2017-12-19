38328413d5a5041e30af60789b6f11775703968a
Jay Turner <jaynicholasturner@gmail.com>
2017-12-19 15:15:41 +0000

    ExRaidGyms Notifications

--- tagger/tagger.py
+++ tagger/tagger.py
@@ -7,14 +7,16 @@ class Tagger:
 	
 	def __init__(self, bot):
 		self.bot = bot
-		self.approved_roles = ('ivysaur','charmeleon','wartortle','metapod','cloyster','tentacruel','sandslash','magneton','marowak','machamp','alakazam','omastar','gengar','scyther','ninetales','porygon','tyranitar','snorlax','golem','lapras','poliwrath','nidoking','nidoqueen','mewtwo','ho-oh','lugia','entei','raikou','zapdos','moltres','celebi','mew','articuno','suicune','absol','mawile','margate','broadstairs','ramsgate','canterbury','hernebay','whitstable','sandwich','ashford', 'groudon')
+		self.approved_roles = ('ivysaur','charmeleon','wartortle','metapod','cloyster','tentacruel','sandslash','magneton','marowak','machamp','alakazam','omastar','gengar','scyther','ninetales','porygon','tyranitar','snorlax','golem','lapras','poliwrath','nidoking','nidoqueen','mewtwo','ho-oh','lugia','entei','raikou','zapdos','moltres','celebi','mew','articuno','suicune','absol','mawile','margate','broadstairs','ramsgate','canterbury','hernebay','whitstable','sandwich','ashford', 'groudon', 'exraidgyms')
+		self.exrgyms = ['Avenue of Remembrance', 'The Downs', 'Herne Meadow', 'Tankerton Coastal Protection Works', 'In Memory of Victor Algar', 'Tankerton Slopes', 'Cannons and Boat Mast', 'Whitstable Castle', 'Chestfield', 'Birchington Play Area', 'Quex Lake Sculptures', 'W and B Golf Club', 'George V. Silver Jubilee Memorial', 'Strokes Adventure Golf', 'Band Rotunda', 'Cliftonville Library at Northdown Park', 'One Day a Woodland', 'Thanet Wanderers RUFC', 'Pierremont Park Water Fountain', 'Ellington Park Bandstand', 'The Waterfall', 'Manufacture of Innovative Medicines', 'Newington Play', 'Water Reservoir', 'Tankerton Skate Park', 'Whitstable Castle Gate House','Reculver Country Park Board']
 	
 	@commands.command(pass_context=True)
 	async def subscribe(self, ctx, species):
+		role = None
 		if species.lower() in self.approved_roles:
 			role = await self.find_role(ctx.message.server, species)
 		if role is None:
-			await self.bot.say("I couldn't find {}. Are you sure it's a valid raid Pokemon?".format(species.capitalize()))
+			await self.bot.say("I couldn't find {}. Are you sure it's a valid role?".format(species.capitalize()))
 			return
 		
 		try:
@@ -23,14 +25,14 @@ class Tagger:
 			await self.bot.edit_message(message, "Error: I don't have permission to set roles. Aborted!")
 			return
 		
-		await self.bot.say("{}, you should find hot fresh {} tags in your inbox soon.".format(ctx.message.author.mention, species.capitalize()))
+		await self.bot.say("{}, you should find hot fresh {} tags in your inbox soon.".format(ctx.message.author.mention, role.name))
 	
 	@commands.command(pass_context=True)
 	async def unsubscribe(self, ctx, species):
 		if species.lower() in self.approved_roles:
 			role = await self.find_role(ctx.message.server, species)
 		if role is None:
-			await self.bot.say("I couldn't find {}. Are you sure it's a valid raid Pokemon?".format(species.capitalize()))
+			await self.bot.say("I couldn't find {}. Are you sure it's a valid role?".format(species.capitalize()))
 			return
 		
 		try:
@@ -39,7 +41,7 @@ class Tagger:
 			await self.bot.edit_message(message, "Error: I don't have permission to set roles. Aborted!")
 			return
 		
-		await self.bot.say("{}, no more {} tags for you!".format(ctx.message.author.mention, species.capitalize()))
+		await self.bot.say("{}, no more {} tags for you!".format(ctx.message.author.mention, role.name))
 	
 	@commands.command(pass_context=True, no_pm=True)
 	@checks.mod_or_permissions(assign_roles=True)
@@ -54,13 +56,12 @@ class Tagger:
 		await self.tags(message)
 	
 	async def find_role(self, server, role_name, create=False):
-		"""All role names are ran through `str.capitalize()`"""
+		"""All role names are compared as `str.lower()`"""
 		
-		role = discord.utils.get(server.roles, name=role_name.capitalize())
+		role = discord.utils.find(lambda x: x.name.lower() == role_name.lower(), server.roles)
 		if not role and create == True:
-			role = await self.bot.create_role(server, name=role_name.capitalize(), mentionable=True)
+			role = await self.bot.create_role(server, name__lower=role_name.lower(), mentionable=True)
 		if role:
-			print(role)
 			return role
 		else:
 			return None
@@ -74,14 +75,20 @@ class Tagger:
 				if len(l.split(':')) == 1:
 					raid['fast_move'], raid['charge_move'] = l.split(' / ')
 				elif len(l.split(':')) == 3:
-					raid['end_time'] = l.split(' ')[3]
+					raid['end_time'] = l.split(' ')[3].lstrip()
 				else:
 					k, v = l.split(':')
-					raid[k] = v
-			new_message = role.mention
+					raid[k] = v.lstrip()
+			try:
+				new_message = role.mention
+			except AttributeError:
+				new_message = message.author.name.split(' ')[0]
 			new_message += ' '+raid['Gym']
 			new_message += ' '+raid['end_time']
 			new_message += '\n'+raid['url']
+			if raid['Gym'] in self.exrgyms:
+				exrrole = await self.find_role(message.server, 'ExRaidGyms')
+				new_message += '\n'+exrrole.mention
 			await self.bot.send_message(message.channel, new_message)
 
 def setup(bot):
